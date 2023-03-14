5cc984a891258c3d958e97fd5bcf70b80a6151ee
Julian Patterson <julian.patterson@gmail.com>
2023-03-14 01:27:26 -0400

    The student.py, teacher.py, and user.py contain super- and subclasses coded in Python that pass 10 superclass and subclass tests.

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,12 @@
 from user import User
 
 class Student(User):
+
+    def __init__(self, first_name=None, last_name=None, knowledge=[None]):
+        print("Teacher.__init__ called with knowledge")
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge
     
-    def learn(self):
+    def learn(self, empty_string=None):
+        self.knowledge.append(empty_string)
         pass
--- lib/teacher.py
+++ lib/teacher.py
@@ -6,5 +6,23 @@ import random
 
 class Teacher(User):
 
+    knowledge = [
+    "str is a data type in Python",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "Python function call definition",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast",
+    ]
+
+    def __init__(self, first_name=None, last_name=None, knowledge=[None]):
+        print("Teacher.__init__ called with knowledge")
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge
+
     def teach(self):
+        random.choice(self.knowledge)
+
         pass
